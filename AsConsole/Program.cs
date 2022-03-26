f1c531b5f2629a29ed4aeda6def40883555325e9
waltiam <2963975+waltiam@users.noreply.github.com>
2022-03-26 20:40:57 +0000

    🎉 it always starts some where

---
+++ AsConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AsConsole/Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+#if DEBUG
+using System;
+using Xunit;
+
+namespace AsConsole
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
+#endif
